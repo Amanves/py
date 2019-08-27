21d443360a4b90983e4ef51e7d5d0d6f47f22bb1
Aman Kumar <48173956+Amanves@users.noreply.github.com>
2019-08-27 17:02:48 +0530

    Add files via upload

---
+++ numpy_linspace.py
@@ -0,0 +1,6 @@
+from numpy import *
+arr = linspace(0,15,16)
+print(arr)
+#all the value is in float because we are breaking the range into parts
+#if we dont specify the parts,it will be 50 parts by deafult
+#(0 -> lower limit,15 -> upper limit,16 -> number of parts)
