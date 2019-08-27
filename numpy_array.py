54e023b23b9a2baa0d0f56d763f573b3993ddef2
Aman Kumar <48173956+Amanves@users.noreply.github.com>
2019-08-27 16:48:35 +0530

    Add files via upload

---
+++ numpy_array.py
@@ -0,0 +1,4 @@
+from numpy import *
+arr = array([2,3,4.0,5,6],int) #here we have specified the type of data,though the original array is float but we have specified it to be int.
+print(arr)
+print(arr.dtype) #it prints the datatype
