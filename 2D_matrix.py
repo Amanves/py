daf58a0959117dc88b8f57a72bb57781b98fbff1
Aman Kumar <48173956+Amanves@users.noreply.github.com>
2019-08-29 21:41:30 +0530

    Add files via upload

---
+++ 2D_matrix.py
@@ -0,0 +1,9 @@
+from numpy import *
+arr1 = array([
+               [2,4,6,7,3,4],
+               [6,4,8,9,0,6]
+            ])
+print(arr1)
+m = matrix(arr1)
+print(m)
+#The output looks same as 2D array,but we with this 'm' we can do several matrix function.
