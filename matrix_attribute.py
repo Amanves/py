a2662aeceb313e5970fd9cba85bb56df22e64e35
Aman Kumar <48173956+Amanves@users.noreply.github.com>
2019-08-29 21:12:21 +0530

    Add files via upload

---
+++ matrix_attribute.py
@@ -0,0 +1,10 @@
+from numpy import *
+arr1 = array([
+               [1,2,3],
+                [4,5,6]
+              ])
+print(arr1)
+print("data type of 2D array = ", arr1.dtype)
+print("Dimension = ", arr1.ndim)
+print("No. of rows and columns = ", arr1.shape)
+print("Size of entire block = ", arr1.size)
