8e5d9fe319ff5a9db606bebe32f1aed70dd78db9
Aman Kumar <48173956+Amanves@users.noreply.github.com>
2019-09-01 23:26:42 +0530

    Add files via upload

---
+++ keyword_arguments.py
@@ -0,0 +1,6 @@
+def person(name,age,gender):
+    print(name)
+    print(age)
+    print(gender)
+person(age=34,name="Aman",gender="Male")
+#incase if we dont know the position of arguments in the function declaration, we can use keyword arguments
